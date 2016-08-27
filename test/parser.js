dafa60438dfda319ec2f4ea915788b3dbabfb479
Enzo Martin <enzo.r.martin@gmail.com>
2016-08-27 18:26:34 +0200

    Add tests for parser module

---
+++ test/parser.js
@@ -0,0 +1,36 @@
+require('./_helper');
+const xmlMock = '<savegame>0.14</savegame>';
+const objMock = {savegame:'0.14'};
+
+const Parser = proxyquire('../lib/parser',{
+    xml2js: {
+        Parser:function Parser(){
+            this.parseString = (str,callback) =>{
+                callback(null,objMock);
+            };
+        },
+        Builder:function Builder(){
+            this.buildObject = () =>{
+                return xmlMock;
+            };
+        }
+    }
+});
+
+
+describe('invoking parser module', () =>{
+    it('should return a JS object converted from the XML', (done) =>{
+        Parser.parse(xmlMock,(err,result) =>{
+            expect(err).toBe(null);
+            expect(typeof result).toEqual('object');
+            expect(result.savegame).toEqual('0.14');
+            done();
+        });
+    });
+
+    it('should compile the passed object into an XML string', () =>{
+        const compiled = Parser.compile(objMock);
+        expect(compiled).toEqual(xmlMock);
+        expect(typeof compiled).toEqual('string');
+    });
+});
