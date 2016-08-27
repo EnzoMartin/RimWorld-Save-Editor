9c73becd6427088855d319fe008d825a5c5fba43
Enzo Martin <enzo.r.martin@gmail.com>
2016-08-27 19:39:45 +0200

    Add tests for utils module - Fix some bugs with the util check functions not looking at the correct data

---
+++ test/mocks/factions.js
@@ -0,0 +1,24 @@
+/* eslint-disable quote-props */
+
+module.exports = [{
+    'def': ['Spacer'],
+    'name': ['Spacer'],
+    'loadID': ['3'],
+    'relations': [{
+        'li': [{
+            'other': ['Faction_9'],
+            'goodwill': ['-100'],
+            'hostile': ['True']
+        }]
+    }]
+},{
+    'def': ['PlayerColony'],
+    'loadID': ['9'],
+    'relations': [{
+        'li': [{
+            'other': ['Faction_3'],
+            'goodwill': ['-100'],
+            'hostile': ['True']
+        }]
+    }]
+}];
